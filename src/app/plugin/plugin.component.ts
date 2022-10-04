dcac35dc428759f617f8400efeacb9f0d4aec80f
MrTigreroux <17600051+MrTigreroux@users.noreply.github.com>
2022-10-04 11:22:21 +0200

    Added russian language for TigerReportsSupports

--- src/app/plugin/plugin.component.ts
+++ src/app/plugin/plugin.component.ts
@@ -15,7 +15,7 @@ export class PluginComponent implements OnInit {
 
   public static readonly PLUGINS_DATA: PluginsData = {
     "TigerReports": { spigotWebLink: "https://www.spigotmc.org/resources/tigerreports.25773/", defaultMessagesLanguage: ["english", "french", "spanish", "chinese", "russian", "vietnamese", "dutch", "german", "polish"] },
-    "TigerReportsSupports": { spigotWebLink: "https://www.spigotmc.org/resources/tigerreportssupports.54612/", defaultMessagesLanguage: ["english", "french", "spanish"] }
+    "TigerReportsSupports": { spigotWebLink: "https://www.spigotmc.org/resources/tigerreportssupports.54612/", defaultMessagesLanguage: ["english", "french", "spanish", "russian"] }
   };
 
   @Input() name: string = "NAME NOT FOUND";
